6c413e6d8e432bfb814c401cf69f64d8dc9c0902
nicholasnrg <15175932+NicholasNRG@users.noreply.github.com>
2026-07-07 19:36:49 -0400

    Implement player rotation using rotate() and keyboard input in update()

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
@@ -17,3 +17,14 @@ class Player(CircleShape):
     
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt: float):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt) # rotate left with negative dt
+        if keys[pygame.K_d]:
+            self.rotate(dt) # rotate right with positive dt
