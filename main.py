1e4cda25c702214a89677bf20d6ef9274cb9012e
nicholasnrg <15175932+NicholasNRG@users.noreply.github.com>
2026-07-12 23:31:50 -0400

    Add asteroid spawning and movement

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import *
+from asteroid import *
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -17,22 +19,28 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            updatable.update(dt)
-
+            
+        updatable.update(dt)
         screen.fill("black")
 
         for sprite in drawable:
             sprite.draw(screen)
-
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
